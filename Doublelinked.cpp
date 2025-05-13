58f759f49e6b6c4ef4db558b8540df8d6827032d
AdamRaditya <Adamradityaakhsan@gmail.com>
2025-05-13 10:02:57 +0700

    add library

---
+++ Doublelinked.cpp
@@ -0,0 +1,15 @@
+// 4NIMBelakang_DoublelinkedList
+#include <iostream>
+using namespace std;
+
+// membuat struktur untuk object double linkedList
+struct Node {
+    // deklarasi noMHS dan name untuk menampung data
+    int noMHS;
+    string name;
+    // deklarasi pointer next dan pref untuk data sebelum dan sesudah
+    Node*next;
+    Node*prev;
+};
+
+// deklarasi pointer START dan pemberian nilai
