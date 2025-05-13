09eb76e8765f35d9c6b5bcc542e806d636df2463
AdamRaditya <Adamradityaakhsan@gmail.com>
2025-05-13 12:11:27 +0700

    membuat void revtraverse

--- Doublelinked.cpp
+++ Doublelinked.cpp
@@ -177,3 +177,22 @@ void traverse()
     }
 }
 
+void revtraverse()
+{
+    if (listempty())
+    cout << "\nList is empty" << endl;
+    else
+    {
+        cout << "\nRecord in descending order of roll number are: " << endl;
+        Node *currentNode = START;
+        while (currentNode->next != NULL)
+            currentNode = currentNode->next;
+
+        while (currentNode != NULL)
+        {
+            cout << currentNode->noMhs << " " << currentNode->name << endl;
+            currentNode = currentNode->prev;
+        }
+    }
+}
+
